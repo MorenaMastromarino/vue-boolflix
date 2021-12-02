a3e157c70115cf4c07caa600d5576bab4890c7b3
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-12-02 17:55:52 +0100

    bandiere

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,15 @@
   <main>
 
     <div v-for="movie in movies" :key="movie.id" class="movie-card">
-       <h3>{{movie.title}}</h3>
-        <p>{{movie.original_title}}</p>
-        <p>{{movie.original_language}}</p>
-        <p>{{movie.vote_average}}</p>     
+        <p><strong>Titolo: </strong>{{movie.title}}</p>
+        <p><strong>Titolo originale: </strong>{{movie.original_title}}</p>
+        <div class="language">
+          <strong>Lingua originale: </strong>
+          <img v-if="movie.original_language === 'en'" src="../assets/img/en.png" alt="english flag">
+          <img v-else-if="movie.original_language === 'it'" src="../assets/img/it.png" alt="italian flag">
+          <span v-else>{{movie.original_language}}</span>          
+        </div>
+        <p><strong>Voto: </strong>{{movie.vote_average}}</p>     
     </div>
          
   </main>
@@ -21,5 +26,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.language img {
+  width: 30px;
+}
 </style>
